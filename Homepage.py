fae175ac0e85f781a6700a8e7b9b9c149797e1c8
Karami <ahmadkarami21@gmail.com>
2023-07-15 19:55:36 +0700

    Added: help tooltips

--- Homepage.py
+++ Homepage.py
@@ -446,7 +446,7 @@ if ss.df_viz_list is not None:
                     # Customer Filter
                     with right_column:
                         predicted_customer_only = st.checkbox("Predicted Customer Only?", value=True,
-                                                              help="Filter Options only shows customer that have"
+                                                              help="Filter Options only shows customer that have "
                                                                    "value in FY2023 (Predicted)")
 
                         if not predicted_customer_only:
